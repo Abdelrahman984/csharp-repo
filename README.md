530072584d733bff2644ccfa4ed44c30997e0799
Abdullah Azmy <abdullahazmy2@gmail.com>
2024-07-29 08:49:37 +0300

    Adding strinDocumentation

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-## C# Basics & Fundmentals
+## C# Basics
 
+> [!NOTE]
+> This assumes that you already have a basic understanding. It focuses on the differences between C# and other languages and highlights some syntaxes.
 
+### DataTypes
+
+C# uses the same data types as C++ and Python, but it is a statically typed language, unlike dynamically typed languages. C# introduces two additional data types compared to C++:
+
+1. `var`: This data type automatically detects the data type.
+2. `decimal`: This data type is similar to `float`, but it offers higher precision with 28-29 significant digits and a size of 128 bits (16 bytes), whereas `float` is only 32 bits (4 bytes) in size. When defining a `decimal` value, you should append `m` after the number, while for `float`, you should append `f`.
+
+All other datatypes are the same as C++.
+
+### Understanding Classes
---
+++ Strings/StringsDocumentation/Program.cs
@@ -0,0 +1,43 @@
+﻿// How to deal with strings in cs
+using System;
+using System.Formats;
+using System.Reflection.Metadata;
+
+namespace Strings
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This will print the number which is refered by {0}, and the currency
+            // prints -- $120.34
+            string myString = string.Format("{0:C}", 120.34);
+            print(myString);
+            
+            // This is a good way for concatination of strings
+            string format = string.Format("{0} canTypehereorleave {1}", "First", "Second");
+            print(format);
+            
+            // Formats long nums
+            //will print 123,121,231,200.000
+            string longNumFormat = string.Format("{0:N}", 123121231200);
+            print(longNumFormat);
+            
+            // show number as percentage
+            // This will print ( The percentage is: 12.300%)
+            string percentNum = string.Format(" The percentage is: {0:P}", .123);
+            print(percentNum);
+            
+            // This will start from index 5 => zero based
+            string mySub = " This shows you an example of a built-in sub function in c# ";
+            mySub = mySub.Substring(6);
+            print(mySub);
+        }
+
+        private static void print(string myString)
+        {
+            Console.WriteLine(myString);
+        }
+    }
+}
+
