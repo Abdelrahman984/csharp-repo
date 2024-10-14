9aa390bbfc635292f05aa1ec11f81318e5dbfaae
Abdullah Azmy <125517573+abdullahazmy@users.noreply.github.com>
2024-10-14 21:44:42 +0300

    Add files via upload

---
+++ assignments/task1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using Task1;
+
+namespace ConsoleApp1;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Student student = new Student() { Id = "2091", Name = "Azmy" };
+        student.print();
+    }
+}
+
+
+
+
---
+++ assignments/task1/Task1/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public void print()
+        {
+            Console.WriteLine($"Id: {Id}, Name: {Name}, Age: {Age}");
+        }
+    }
+}
